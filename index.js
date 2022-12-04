e2f254e99bf3e38b7620721e4590e598f097fd1b
phalashi <77202298+phalashi@users.noreply.github.com>
2022-12-04 18:44:01 +0530

    added cosmetics and cosmeticslist

--- index.js
+++ index.js
@@ -68,32 +68,35 @@ app.get('/signupsubmit', (req, res) => {
 
 app.get('/query',function(req,res){
 
-    request('https://api.spoonacular.com/recipes/complexSearch?query='+req.query.query+'&apiKey=1bc3c5a2309b4f3e8411af3de8988074',
-    function(error,response,body){
+    request("http://makeup-api.herokuapp.com/api/v1/products.json?product_type=" + req.query.search,
+    function(error,response,body) {
         if(JSON.parse(body).number > 0){
-            const q = req.query.query;
-            const img0 = JSON.parse(body).results[0].image;
-            const img1 = JSON.parse(body).results[1].image;
-            const img2 = JSON.parse(body).results[2].image;
-            const img3 = JSON.parse(body).results[3].image;
-            const img4 = JSON.parse(body).results[4].image;
-            const img5 = JSON.parse(body).results[5].image;
-            const img6 = JSON.parse(body).results[6].image;
-            const img7 = JSON.parse(body).results[7].image;
-            const img8 = JSON.parse(body).results[8].image;
-            const title0 = JSON.parse(body).results[0].title;
-            const title1 = JSON.parse(body).results[1].title;
-            const title2 = JSON.parse(body).results[2].title;
-            const title3 = JSON.parse(body).results[3].title;
-            const title4 = JSON.parse(body).results[4].title;
-            const title5 = JSON.parse(body).results[5].title;
-            const title6 = JSON.parse(body).results[6].title;
-            const title7 = JSON.parse(body).results[7].title;
-            const title8 = JSON.parse(body).results[8].title;
-            res.render('foodrecipe',{q:q,img0:img0,img1:img1,img2:img2,img3:img3,img4:img4,img5:img5,img6:img6,img7:img7,img8:img8,title0:title0,title1:title1,title2:title2,title3:title3,title4:title4,title5:title5,title6:title6,title7:title7,title8:title8})
+            const q = req.query.search;
+
+            let imagelinks = [];
+            let productlinks = [];
+            let productnames = [];
+            let prices = [];
+            for (var i = 0; i < 10; i++)
+            { 
+                imagelinks.push(JSON.parse(body).results[i].image_link);
+                productlinks.push(JSON.parse(body).results[i].product_link);
+                productnames.push(JSON.parse(body).results[i].name);
+                prices.push(JSON.parse(body).results[i].price);
+
+            }
+
+           
+            res.render('cosmeticlist',{
+                q:q,
+                name1:productnames[0], name2:productnames[1], name3:productnames[2], name4:productnames[3], name5:productnames[4], name6:productnames[5], name7:productnames[6], name8:productnames[7], name9:productnames[8], name10:productnames[9],
+                image1:imagelinks[0], image2:imagelinks[1], image3:imagelinks[2], image4:imagelinks[3], image5:imagelinks[4], image6:imagelinks[5], image7:imagelinks[6], image8:imagelinks[7], image9:imagelinks[8], image10:imagelinks[9], 
+                prod1:productlinks[0], prod2:productlinks[1], prod3:productlinks[2], prod4:productlinks[3], prod5:productlinks[4], prod6:productlinks[5], prod7:productlinks[6], prod8:productlinks[7], prod9:productlinks[8], prod10:productlinks[9], 
+                price1:prices[0], price2:prices[1], price3:prices[2], price4:prices[3], price5:prices[4], price6:prices[5], price7:prices[6], price8:prices[7], price9:prices[8], price10:prices[9]
+            })
         }
         else{
-            res.send("smtg went wrong");
+            res.send("error fetching results!");
         }
     });
     
